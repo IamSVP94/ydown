8d5ddfa3e4e34ab3def8ba60ea76f2de6f820882
jubick1337 <Lytdybr99>
2021-02-05 16:14:32 +0500

    updated argparser

--- Yandex.Disk_downloader.py
+++ Yandex.Disk_downloader.py
@@ -1,17 +1,15 @@
 import argparse
 import json
 import os
+
 import requests
-import sys
 
-
-def get_params():
-    parser = argparse.ArgumentParser(description='This utility allows download files and dirs from Yandex.Disk.')
-    parser.add_argument("-d", default=False, action='store_const', const=True,
-                        help='Wget downloading files if you pointed flag')
-    parser.add_argument("--url", required=True, help='Paste download link from Yandex.Disk here')
-    params = parser.parse_args(sys.argv[1:])
-    return params
+parser = argparse.ArgumentParser(
+    description='This utility allows to download or create downloading links '
+                'for files and dirs from Yandex.Disk via public link')
+parser.add_argument("-d", default=False, action='store_const', const=True,
+                    help='Wget downloading files if you pointed flag')
+parser.add_argument("--url", required=True, help='Paste download link from Yandex.Disk here')
 
 
 def get_link(public_key):
@@ -23,7 +21,7 @@ def get_link(public_key):
 
 
 if __name__ == '__main__':
-    params = get_params()
+    params = parser.parse_args()
     link = get_link(params.url)
     if params.d:
         os.system(f'wget {link}')
